4880db190f28886f7b2cbf264e0007d5795debdd
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 <dseo@c6r3s6.42seoul.kr>
2022-11-11 15:55:03 +0900

    get_next_line

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dseo <dseo@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/12 16:41:59 by dseo              #+#    #+#             */
+/*   Updated: 2022/08/24 05:49:06 by dseo             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+char	*ft_strdup(const char *s1);
+char	*ft_strjoin(char *s1, char *s2);
+size_t	ft_strlen(const char *s);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcpy(char *dst, const char *src,
+			size_t dstsize);
+
+#endif
